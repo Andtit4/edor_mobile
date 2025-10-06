7145e1ad59e6800e905525263c846a09e2b1377b
Andtit4 <lirsitogo2021@gmail.com>
2025-10-06 11:48:17 +0200

    Remove sensitive Firebase service account key from key.json and update API base URL in app_config.dart to a new local address for development.

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -2,8 +2,8 @@
 import 'dart:io';
 
 class AppConfig {
-  static String _apiBaseUrl = 'http://185.97.146.99:8090';
-  // static String _apiBaseUrl = 'http://localhost:8090';
+  //static String _apiBaseUrl = 'http://185.97.146.99:8090';
+  static String _apiBaseUrl = 'http://192.168.100.34:8090';
   //static String _apiBaseUrl = 'http://192.168.1.71:8090';
   static String _appName = 'Edor';
   static String _appVersion = '1.0.0';
